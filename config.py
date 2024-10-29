7209392c4c2a388b6cd8c09fbde8c513cc5ffdf6
chailana <chailana35217793@gmail.com>
2024-10-29 21:46:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-API_ID = 123456  # api id
-API_HASH = "ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # api hash
+API_ID = 22420997  # api id
+API_HASH = "d7fbe2036e9ed2a1468fad5a5584a255"  # api hash
 
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
+BOT_TOKEN = "7176424785:AAEusrLtmtGgRisJ6Pje6yAnN-ZbdZMoO1Q"  # bot token
 
 
 # REDIS
@@ -11,11 +11,7 @@ PASSWORD = ""  # redis password
 
 PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
 # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=;
-COOKIE = "# Netscape HTTP Cookie File
-# http://curl.haxx.se/rfc/cookie_spec.html
-# This is a generated file!  Do not edit.
-
-www.terabox.com	FALSE	/	FALSE	0	csrfToken	8ciucK7IIgedx5de0-G4rdRo
+COOKIE = "www.terabox.com	FALSE	/	FALSE	0	csrfToken	8ciucK7IIgedx5de0-G4rdRo
 .terabox.com	TRUE	/	FALSE	1735402209	browserid	F3FeCFwScbFMj2EXAzGmYKveK8Au-oqO6Ie7wcCgMkJKuguvB7cysw_oh18=
 .terabox.com	TRUE	/	FALSE	1732810295	lang	en
 .terabox.com	TRUE	/	FALSE	1764778303	__bid_n	192d90ab7e7718e7b74207
@@ -27,12 +23,12 @@ www.terabox.com	FALSE	/	FALSE	1732810300	ndut_fmt	0586AEF9A26CFBB5DC48321B413D3F
 .terabox.com	TRUE	/	TRUE	1730225504	ab_sr	1.0.1_ZjM2YmRkYWM3ZWRjZjVhOTQ0MmNlMjhhNTBhZTgwNGM0YWQxOTUyNGY5ZjUzOWRjZDUwNzQxN2QzMmJlYjI2N2RjZDI2ODM3OWYxYWVkNjQ5ODZhYTE2YzBhNzczYTI1MTNkZmEwN2JhMmUxNGZiNjM2ZWMyZmYyMGJmNjU2NWU5MzIxY2I2NzJmNjE5OGYxYjUxMzRjOWY4M2I2M2FhYw==
 .terabox.com	TRUE	/	FALSE	0	ab_ymg_result	{"data":"c562aab96aeaa6ed74b2b8b771d653c3b5fb4b36ca744077a28eeb34741e18c6d47485f3d285b0a85ed33898c40759ed2bfb958c0c63c09e3d490fbe522ef9477e939f7d15f12210aafd1150631622c185717fed178ad8dc7394bbf9b45c57ee6c65b5a2aaea0eb4aea28b12ac83c6af7045e058430797e09dbc3dcb82d5fa16","key_id":"66","sign":"88fdf420"}
 "
-ADMINS = [1317173146]
+ADMINS = [7170426058]
 
 
-BOT_USERNAME = "teraboxdown_bot"
+BOT_USERNAME = "anuthing9_bot"
 
 # Force user to join this channel. (make sure you have promoted the bot on this chat.)
-FORCE_LINK = "@RoldexVerse"
+FORCE_LINK = "@anime"
 
 PUBLIC_EARN_API = ""  # https://publicearn.com/api
